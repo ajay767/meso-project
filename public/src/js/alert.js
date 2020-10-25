90e276818353b12a6c7e037e8c217558e0067ee1
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-10-25 17:37:09 +0530

    minor bug fix

--- public/src/js/alert.js
+++ public/src/js/alert.js
@@ -1,10 +1,11 @@
-const warningMarkup = `<div class="alert__box alert__warning"><svg><use xlink:href="src/img/icon-pack-2.svg#danger"></use></svg><p class="primary__heading">Sorry the service is corrently disabled!</p></div>`;
-const successMarkup = `<div class="alert__box alert__success"><svg><use xlink:href="src/img/icon-pack-2.svg#happy"></use></svg><p class="primary__heading">Yeahh now you are part of our family.</p></div>`;
-const errorMarkup = `<div class="alert__box alert__error"><svg><use xlink:href="src/img/icon-pack-2.svg#sad"></use></svg><p class="primary__heading">Something wrong has happened!</p></div>`;
+const warningMarkup = `<div class="alert__box alert__warning"><svg><use xlink:href="src/img/icon-pack-2.svg#danger"></use></svg><p class="primary__heading">MESSAGE</p></div>`;
+const successMarkup = `<div class="alert__box alert__success"><svg><use xlink:href="src/img/icon-pack-2.svg#happy"></use></svg><p class="primary__heading">MESSAGE</p></div>`;
+const errorMarkup = `<div class="alert__box alert__error"><svg><use xlink:href="src/img/icon-pack-2.svg#sad"></use></svg><p class="primary__heading">MESSAGE</p></div>`;
 
-function showAlert(typeOfAlert) {
+function showAlert(typeOfAlert, msg) {
+  const alertMsg = typeOfAlert.replace('MESSAGE', msg);
   const header = document.querySelector('.navigation');
-  header.insertAdjacentHTML('beforeend', typeOfAlert);
+  header.insertAdjacentHTML('beforeend', alertMsg);
   setTimeout(() => {
     document.querySelector('.alert__box').classList.add('showAlert');
   }, 400);
@@ -22,10 +23,10 @@ document
     console.log(value);
 
     value === 'success'
-      ? showAlert(successMarkup)
+      ? showAlert(successMarkup, 'hum honge kamyabbb ek din!!')
       : value === 'error'
-      ? showAlert(errorMarkup)
-      : showAlert(warningMarkup);
+      ? showAlert(errorMarkup, 'Beta ERROR aarha h!!')
+      : showAlert(warningMarkup, 'Hey! you have got a warning!');
 
     setTimeout(() => {
       hideAlert();
