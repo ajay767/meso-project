420e545f41eb86de0665858f4b54df68e4696a6a
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-11-02 19:35:06 +0530

    font-correction

--- public/src/js/bundle.js
+++ public/src/js/bundle.js
@@ -12355,7 +12355,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49780" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60116" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
